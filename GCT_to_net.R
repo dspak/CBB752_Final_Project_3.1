e33853bbf277616b6b57d29b2e5fc065266dbe49
Dan Spakowicz <daniel.spakowicz@yale.edu>
2016-04-29 15:42:44 -0400

    cleaned up for cmd line

--- GCT_to_net.R
+++ GCT_to_net.R
@@ -14,8 +14,8 @@
 # Updated.by    : 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Usage:      Rscript GCT_to_net.R /path/to/inputfile.gct /path/to/outputfile.csv
-# Example:    python GCT_to_net.py -i input.gct -p 0.01 -o output.csv
+# Usage:      Rscript GCT_to_net.R --input /path/to/inputfile.gct --pvalue 0.05 -output /path/to/outputfile.csv
+# Example:    python GCT_to_net.py -i input.gct -p 0.05 -o output.csv
 # Note:       Input: GCT file, which is a text file with 1st row = column titles
 #             Columns: Name, Description, Experiment1, Experiment 2, exp3, exp4 ...
 #             One row per gene
@@ -24,39 +24,87 @@
 #             example.gct: constructed from all_aml_train.gct, remove 2 header lines, keep first 10 genes
 
 # Load the required packages
-#install.packages("argparser")
-library(argparser)
+if(!require(optparse)){install.packages("optparse")}
 if(!require(Hmisc)){install.packages("Hmisc")}
 
+# set arguments
+option_list = list(
+  make_option(c("-i", "--input"), type="character", default=NULL, 
+              help="dataset file name", metavar="character"),
+  make_option(c("-p", "--pvalue"), type="character", default=0.05, 
+              help="input pvalue between 0 and 1", metavar="character"),
+  make_option(c("-o", "--out"), type="character", 
+              default="output_coexpressed_R.csv", 
+              help="output file name [default= %default]", metavar="character")
+); 
 
-# Set to run as Rscript GCT_to_net.R /path/to/inputfile.gct
-# To run interactively, use the setwd and input files below
-args = commandArgs(trailingOnly=TRUE)
-argv <- parse_args(p)
-
-
-# specify our desired options
-# by default ArgumentParser will add an help option
-i <- add_argument(i, "--input", default=TRUE,
-                    help='input file, tab delimited in a .txt file, MI TAB 2.5 format')
-parser$add_argument("-p", "--pvalue", action="store_false", default=TRUE,
-                    dest="verbose", help='input pvalue between 0 and 1')
-parser$add_argument("-o", "--output", action="store_false",
-                    dest="verbose", help='output filename')
-
-input <- read.csv(file = args[1], header = T, sep = "\t", strip.white = T, quote = "\"", stringsAsFactors = F)
-
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
 
 # To run interactively, uncomment the following two lines
-setwd("~/Box Sync/coursework/CBB752_BioinformaticsMiningSimulation/final/CBB752_Final_Project_3.1/")
-input <- read.table(file = "all_aml_train.preprocessed.gct", header = T, sep = "\t", strip.white = T, quote = "\"", stringsAsFactors = F, row.names = 1)
+# setwd("~/Box Sync/coursework/CBB752_BioinformaticsMiningSimulation/final/CBB752_Final_Project_3.1/")
+# infile <- read.table(file = "all_aml_train.preprocessed.gct", header = T, sep = "\t", strip.white = T, quote = "\"", stringsAsFactors = F, row.names = 1)
 
-
-
-
-### Excluded from final product, include if using raw (not preprocessed) data
 # ~~~~~~~~~~~~~
-# ~~ STEP 1) Apply low and high thresholds and remove values that never change
+# ~~ Find co-expressed genes by Pearson correlation, output table
+# ~~~~~~~~~~~~~
+
+PearsonCorrelationTable <- function(infile, pvalue, outfile){
+  
+  # read in data matrix
+  infile <- read.csv(file = opt$input, header = T, sep = "\t", 
+                     strip.white = T, quote = "\"", stringsAsFactors = F)
+  
+  # pearson correlation excluding name columns using Hmisc
+  r <- rcorr(t(infile[,3:ncol(input)]), type = "pearson")
+  
+  # reformat matrix to two columns of interactions with values
+  df <- flattenCorrMatrix(r$r, r$P)
+  
+  # correct for multiple hypothesis testing
+  df$bonferroni <- p.adjust(df$p, method = "bonferroni")
+  
+  # calculate the t-statistic to include in the final output
+  df$t.statistic <- df$cor / (sqrt( (1-(df$cor^2)/(ncol(infile)-4) )))
+  
+  # subset the result to only significant correlations
+  significants <- subset(df, bonferroni <= pvalue)
+  
+  # reformat output
+  output <- data.frame(significants$row, significants$column, significants$cor, 
+                        significants$t.statistic, significants$p, 
+                        significants$bonferroni)
+  colnames(output) <- c("InteractorA", "InteractorB", "Correlation",
+                        "t-statistic", "p-value", "Bonferroni-adjusted p-value")
+  
+  # write to file
+  write.csv(output, outfile, 
+            row.names = F, quote = F, col.names = T)
+}
+
+# function to reformat matrix to two columns of interactions with values
+flattenCorrMatrix <- function(cormat, pmat) {
+  ut <- upper.tri(cormat)
+  data.frame(
+    row = rownames(cormat)[row(cormat)[ut]],
+    column = rownames(cormat)[col(cormat)[ut]],
+    cor  =(cormat)[ut],
+    p = pmat[ut]
+  )
+}
+
+
+PearsonCorrelationTable(opt$input, opt$pvalue, opt$out)
+
+
+
+
+
+# http://stats.stackexchange.com/questions/153937/finding-p-value-in-pearson-correlation-in-r
+
+#Excluded from final version, include if using unpreprocessed data
+# ~~~~~~~~~~~~~ 
+# ~~ Apply low and high thresholds and remove values that never change
 # ~~~~~~~~~~~~~
 # This requires first cleaning the data. As done using GenePattern earlier in this assignment, values <20 will be set to 20, values > 20000 will be set to 20000, and values that change by less than 3 fold between any sample will be removed.
 # 
@@ -73,43 +121,6 @@ input <- read.table(file = "all_aml_train.preprocessed.gct", header = T, sep = "
 # df.desc <- cbind(df.low$Name, df.low$Description, df.low.high)
 # df.clean <- df.desc[df.low.high$folddiff > 3,]
 # df.clean$folddiff <- NULL
-
-
-# ~~~~~~~~~~~~~
-# ~~ STEP 2) Find co-expressed genes by Pearson correlation
-# ~~~~~~~~~~~~~
-
-
-
-n <- ncol(input)-2
-
-# http://stats.stackexchange.com/questions/153937/finding-p-value-in-pearson-correlation-in-r
-
-r <- rcorr(t(input[,3:ncol(input)]), type = "pearson")
-
-flattenCorrMatrix <- function(cormat, pmat) {
-  ut <- upper.tri(cormat)
-  data.frame(
-    row = rownames(cormat)[row(cormat)[ut]],
-    column = rownames(cormat)[col(cormat)[ut]],
-    cor  =(cormat)[ut],
-    p = pmat[ut]
-  )
-}
-df <- flattenCorrMatrix(r$r, r$P)
-df$bonferroni <- p.adjust(df$p, method = "bonferroni")
-df$t.statistic <- df$cor / (sqrt( (1-(df$cor^2)/(ncol(input)-4) )))
-
-output <- subset(df, bonferroni <= 0.05)
-output2 <- data.frame(output$row, output$column, output$cor, output$t.statistic, output$p, output$bonferroni)
-colnames(output2) <- c("InteractorA", "InteractorB", "Correlation","t-statistic", "p-value", "Bonferroni-adjusted p-value")
-write.csv(output2, "output_coexpressed_R.csv", row.names = F, quote = F, col.names = T)
-
-
-
-
-
-
 
 
 
@@ -139,6 +150,3 @@ write.csv(output2, "output_coexpressed_R.csv", row.names = F, quote = F, col.nam
 # 
 # # find p-value for the double-sided test. Students t, n-2 degrees of freedom
 # pval = stats.t.sf(numpy.abs(tstat), n-2)*2
-# 
-# 
-# 
